6678112821a7ccc3b9719fa7db451eacb9a44719
Katie Blizzard <kblizzard11@gmail.com>
2019-02-09 18:00:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,8 @@ def my_each(array)
   if block_given?
     i = 0 
     while i < array.length 
-      yield(array[i])
+      yield
       i += 1 
-      return array
     end
   else
     return array
