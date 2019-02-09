5afc895ac35cced26459d11eb87e067ccf8e94d9
Katie Blizzard <kblizzard11@gmail.com>
2019-02-09 17:30:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,24 +9,3 @@ def my_each(array)
   else
     "No block was given"
   end
-  
-  
-my_each(array) { }
-
-
-end
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
